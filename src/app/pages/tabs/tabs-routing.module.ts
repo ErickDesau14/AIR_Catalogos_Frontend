dc7084d8c880aa79d094403a6ce71815a0b89de6
Erick de la Cruz <erick_1322134389@uptecamac.edu.mx>
2024-10-30 13:27:22 -0600

    Estrucutra de páginas (tabs) y borrado del modo oscuro predeterminado de angular

--- src/app/pages/tabs/tabs-routing.module.ts
+++ src/app/pages/tabs/tabs-routing.module.ts
@@ -9,15 +9,19 @@ const routes: Routes = [
     children: [
       {
         path: 'tab1',
-        loadChildren: () => import('../tab1/tab1.module').then(m => m.Tab1PageModule)
+        loadChildren: () => import('../tecnologias/tab1/tab1.module').then(m => m.Tab1PageModule)
       },
       {
         path: 'tab2',
-        loadChildren: () => import('../tab2/tab2.module').then(m => m.Tab2PageModule)
+        loadChildren: () => import('../puestos/tab2/tab2.module').then(m => m.Tab2PageModule)
       },
       {
         path: 'tab3',
-        loadChildren: () => import('../tab3/tab3.module').then(m => m.Tab3PageModule)
+        loadChildren: () => import('../experiencia/tab3/tab3.module').then(m => m.Tab3PageModule)
+      },
+      {
+        path: 'tab4',
+        loadChildren: () => import('../modalidad/tab4/tab4.module').then(m => m.Tab4PageModule)
       },
       {
         path: '',
