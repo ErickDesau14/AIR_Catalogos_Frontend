49e67e9b7f7f329b7329aa832ddcbffbc5a06f98
Erick de la Cruz <erick_1322134389@uptecamac.edu.mx>
2024-10-29 20:41:07 -0600

    Instalación de sqlite (Temporal)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Device } from '@capacitor/device';
+import { Platform } from '@ionic/angular';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,28 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+
+  public isWeb: boolean;
+
+  constructor(
+    private platform: Platform,
+    private translate: TranslateService
+  ) {
+    this.translate.setDefaultLang('es');
+    this.initApp();
+  }
+
+  initApp() {
+    this.platform.ready().then( async () => {
+      const language = Device.getLanguageCode();
+      const info = await Device.getInfo();
+
+      this.isWeb = info.platform == 'web';
+
+      if ((await language).value) {
+        this.translate.use((await language).value.slice(0, 2));
+      }
+    })
+  }
+
 }
