cce8417e2f9d8f3680b2e78159cfacbd438a95df
Erick de la Cruz <erick_1322134389@uptecamac.edu.mx>
2024-11-28 13:55:05 -0600

    Añadiendo página de catálogos de modalidad

--- src/app/pages/modalidad/tab4/tab4.module.ts
+++ src/app/pages/modalidad/tab4/tab4.module.ts
@@ -7,14 +7,19 @@ import { IonicModule } from '@ionic/angular';
 import { Tab4PageRoutingModule } from './tab4-routing.module';
 
 import { Tab4Page } from './tab4.page';
+import { TranslateModule } from '@ngx-translate/core';
+import { ListDataModeComponent } from 'src/app/shared/list-data-mode/list-data-mode.component';
+import { ListModeComponent } from './components/list-mode/list-mode.component';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    Tab4PageRoutingModule
+    Tab4PageRoutingModule,
+    TranslateModule.forChild(),
+    ListDataModeComponent
   ],
-  declarations: [Tab4Page]
+  declarations: [Tab4Page, ListModeComponent]
 })
 export class Tab4PageModule {}
