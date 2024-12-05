b32fb4b6be1f9d62b02b2d1d65c2b86365c989f7
Erick de la Cruz <erick_1322134389@uptecamac.edu.mx>
2024-12-05 16:51:42 -0600

    Creando componentes de notificaciones en shared

---
+++ src/app/shared/list-data-notifications/list-data-notifications.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-data-notifications',
+  templateUrl: './list-data-notifications.component.html',
+  styleUrls: ['./list-data-notifications.component.scss'],
+})
+export class ListDataNotificationsComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
---
+++ src/app/shared/list-data-type-notifications/list-data-type-notifications.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-data-type-notifications',
+  templateUrl: './list-data-type-notifications.component.html',
+  styleUrls: ['./list-data-type-notifications.component.scss'],
+})
+export class ListDataTypeNotificationsComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
