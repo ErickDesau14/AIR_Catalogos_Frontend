5b4ab06b0f54a2c8690b9a9e6a3e8483b27a0e2f
Erick de la Cruz <erick_1322134389@uptecamac.edu.mx>
2024-12-05 15:51:40 -0600

    Añadiendo modelo de catálogo de notificaciones

---
+++ src/app/models/notifications.ts
@@ -0,0 +1,8 @@
+export class Notificaciones {
+  id: number;
+  texto: string;
+  estatus: number;
+  fechaCreacion: Date;
+  fechaModificacion: Date;
+  fechaBaja: Date;
+}
---
+++ src/app/models/typeNotifications.ts
@@ -0,0 +1,8 @@
+export class TipoNotificacciones {
+  idTipoNotificacion: number;
+  tipoNotificacion: string;
+  estatus: number;
+  fechaCreacion: Date;
+  fechaModificacion: Date;
+  fechaBaja: Date;
+}
