9b0e1e5aeb282cf9c860b39cb2241c4dab01b9ef
Erick de la Cruz <erick_1322134389@uptecamac.edu.mx>
2024-12-10 11:47:57 -0600

    tecnologia service completo

--- src/app/services/tecnologia.service.ts
+++ src/app/services/tecnologia.service.ts
@@ -2,18 +2,37 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Tecnologias } from '../models/tecnologias';
 import { environment } from 'src/environments/environment';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TecnologiaService {
 
+  private baseUrl = `${environment.tecnologiaUrl}/tecnologias`;
+
   constructor(
     private httpClient : HttpClient,
   ) { }
 
-  getTecnologiasService() {
-    return this.httpClient.get<Tecnologias[]>(`${environment.tecnologiaUrl}/tecnologias`);
+  getTecnologiaById(id: number): Observable<Tecnologias> {
+    return this.httpClient.get<Tecnologias>(`${environment.tecnologiaUrl}/tecnologias/${id}`);
+  }
+
+  getTecnologias(): Observable<Tecnologias[]> {
+    return this.httpClient.get<Tecnologias[]>(this.baseUrl);
+  }
+
+  createTecnologia(tecnologia: Tecnologias): Observable<Tecnologias> {
+    return this.httpClient.post<Tecnologias>(this.baseUrl, tecnologia);
+  }
+
+  updateEstatus(id: number, estatus: number): Observable<Tecnologias> {
+    return this.httpClient.put<Tecnologias>(`${environment.tecnologiaUrl}/tecnologias/${id}/estatus/${estatus}`, {});
+  }
+
+  updateTecnologia(id: number, tecnologia: Tecnologias): Observable<Tecnologias> {
+    return this.httpClient.put<Tecnologias>(`${environment.tecnologiaUrl}/tecnologias/${id}`, tecnologia);
   }
 
 }
