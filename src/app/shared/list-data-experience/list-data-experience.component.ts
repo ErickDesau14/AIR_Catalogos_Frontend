6ae7f05417d4157c65500e71dbfaeea9d824cb8f
Erick de la Cruz <erick@kohmi.net>
2024-12-12 09:24:28 -0600

    Creación de funciones de validaciones y añadir año de experiencia

--- src/app/shared/list-data-experience/list-data-experience.component.ts
+++ src/app/shared/list-data-experience/list-data-experience.component.ts
@@ -3,7 +3,9 @@ import { Component, ContentChild, EventEmitter, Input, Output, TemplateRef } fro
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { TranslateModule } from '@ngx-translate/core';
+import { Experiencia } from 'src/app/models/experiencie';
 import { AlertService } from 'src/app/services/alert.service';
+import { ExperienciaService } from 'src/app/services/experience.service';
 
 @Component({
   selector: 'app-list-data-experience',
@@ -20,46 +22,95 @@ export class ListDataExperienceComponent {
   @Input() showAdd: boolean = true;
   @Input() isEditing: boolean = false;
 
-  @Input() yearExperience: string = '';
+  @Input() yearExperience: number = 0;
   @Input() creationDate: string = '';
   @Input() modificationDate: string = '';
   @Input() deactivationDate: string = '';
   @Input() isReadOnly: boolean = true;
-  @Input() selectedYearExperience: string = '';
+  @Input() selectedYearExperience: number = 0;
 
   @Output() experienceAdded: EventEmitter<void> = new EventEmitter<void>();
-  @Output() updateYearExperience = new EventEmitter<string>();
+  @Output() updateYearExperience = new EventEmitter<number>();
+  @Output() resetForm = new EventEmitter<void>();
 
   @ContentChild('templateData', { static: false})
   templateData: TemplateRef<any>;
 
   constructor(
-    private alertService: AlertService
+    private alertService: AlertService,
+    private experienciaService: ExperienciaService
   ) { }
 
+  private validateExperienceYear(year: number): boolean {
+    if (!year || year <= 0) {
+      this.alertService.alertWarning('El año de experiencia no puede ser menor o igual a 0');
+      return false;
+    }
+
+    const exists = this.data.some((exp: Experiencia) => exp.anho === year);
+
+    if (exists) {
+      this.alertService.alertWarning('Este año de experiencia ya existe');
+      return false;
+    }
+
+    return true;
+  }
+
   get isYearExperienceValid(): boolean {
-    return this.yearExperience && this.yearExperience.trim().length > 0;
+    return this.selectedYearExperience > 0;
+  }
+
+  emmitUpdateYearExperience() {
+    if (!this.validateExperienceYear(this.selectedYearExperience)) {
+      return;
+    }
+
+    this.updateYearExperience.emit(this.selectedYearExperience);
   }
 
   addData() {
 
-    if (!this.yearExperience) {
-      this.alertService.alertError(
-        'El año de experiencia no puede estar vacío',
-      );
+    if (!this.validateExperienceYear(this.selectedYearExperience)) {
       return;
     }
 
-    // const normalizedYearExperience = this.yearExperience.replace(/\s+/g, '').toLowerCase();
+    const newExperience: Experiencia = {
+      anho: this.selectedYearExperience,
+      estatus: 1
+    };
 
-        // this.sqliteManager.yearExperienceExists(normalizedYearExperience)
-    // .then((exists) => {
-    //   if (exists) {
-    //     this.alertService.alertWarning('Este año de expriencia ya existe');
-    //     return;
-    //   }
-    // });
+    this.alertService.alertConfirm(
+      '¿Estás seguro de que deseas agregar el año de experiencia?',
+      () => {
+        this.experienciaService.createExperiencia(newExperience).subscribe({
+          next: () => {
+            this.alertService.alertSuccess('Año de experiencia agregado correctamente');
+            this.resetFormFields();
+            this.experienceAdded.emit();
+          },
+          error: async (err) => {
+            if (err.error && err.error.message) {
+              await this.alertService.alertWarning(err.error.message);
+              this.resetFormFields();
+            } else {
+              await this.alertService.alertError('Error al agregar el año de experiencia ' + err.message);
+            }
+          }
+        })
+      }
+    )
 
+  }
+
+  resetFormFields() {
+    this.selectedYearExperience = 0;
+    this.creationDate = '';
+    this.modificationDate = '';
+    this.deactivationDate = '';
+    this.isEditing = false;
+    this.isReadOnly = false;
+    this.resetForm.emit();
   }
 
 }
