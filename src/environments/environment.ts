34feb76ae8b6fa2a822ac283eeee08e7f4a91f1c
Erick de la Cruz <erick@kohmi.net>
2024-12-11 22:49:31 -0600

    añadiendo ruta backend de experiencia

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,6 +7,7 @@ export const environment = {
   recursoUrl: 'http://localhost:8080/api/v1/r',
   tecnologiaUrl: 'http://localhost:8080/api/v1/t',
   puestoUrl: 'http://localhost:8080/api/v1/p',
+  experienciaUrl: 'http://localhost:8080/api/v1/e',
   empresaUrl: 'http://localhost:8080/api/v1/e',
   archivoUrl: 'http://localhost:8080/api/v1/a/recurso',
   evaluacionUrl: 'http://localhost:8080/api/v1/e/recurso'
