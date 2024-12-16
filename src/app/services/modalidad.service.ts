4824b09927baa39b1aa11fb42482b2b1d5d3765c
Erick de la Cruz <erick@kohmi.net>
2024-12-16 12:33:11 -0600

    Creando servicio de modalidad

--- src/app/services/modalidad.service.ts
+++ src/app/services/modalidad.service.ts
@@ -1,6 +1,5 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { Puestos } from '../models/positions';
 import { Observable } from "rxjs";
 import { environment } from "src/environments/environment";
 import { Modalidad } from "../models/mode";
@@ -8,16 +7,16 @@ import { Modalidad } from "../models/mode";
 @Injectable({
   providedIn: 'root'
 })
-export class PuestoService {
+export class ModalidadService {
 
-  private baseUrl = `${environment.modalidadUrl}/modalidades`;
+  private baseUrl = `${environment.modalidadUrl}/modalidad`;
 
   constructor(
     private httpClient: HttpClient
   ) {}
 
   getModalidadById(id: number): Observable<Modalidad> {
-    return this.httpClient.get<Modalidad>(`${environment.modalidadUrl}/modalidades/${id}`);
+    return this.httpClient.get<Modalidad>(`${environment.modalidadUrl}/modalidad/${id}`);
   }
 
   getModalidadService() {
@@ -33,11 +32,11 @@ export class PuestoService {
   }
 
   updateEstatus(id: number, estatus: number): Observable<Modalidad> {
-    return this.httpClient.put<Modalidad>(`${environment.modalidadUrl}/modalidades/${id}/estatus/${estatus}`, {});
+    return this.httpClient.put<Modalidad>(`${environment.modalidadUrl}/modalidad/${id}/estatus/${estatus}`, {});
   }
 
   updateModalidad(id: number, modalidad: Modalidad): Observable<Modalidad> {
-    return this.httpClient.put<Modalidad>(`${environment.modalidadUrl}/modalidades/${id}`, modalidad);
+    return this.httpClient.put<Modalidad>(`${environment.modalidadUrl}/modalidad/${id}`, modalidad);
   }
 
 }
