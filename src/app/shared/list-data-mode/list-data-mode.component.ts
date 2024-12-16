51dd2fcbf9ba99e3e7d3fc2f62d3c02d1818213c
Erick de la Cruz <erick@kohmi.net>
2024-12-16 12:37:46 -0600

    Añadiendo nuevo output para reset form y desarrollo de funciones de nomarlizacion de nombre y gregar modaldiad

--- src/app/shared/list-data-mode/list-data-mode.component.ts
+++ src/app/shared/list-data-mode/list-data-mode.component.ts
@@ -3,7 +3,9 @@ import { Component, ContentChild, EventEmitter, Input, Output, TemplateRef } fro
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { TranslateModule } from '@ngx-translate/core';
+import { Modalidad } from 'src/app/models/mode';
 import { AlertService } from 'src/app/services/alert.service';
+import { ModalidadService } from 'src/app/services/modalidad.service';
 
 @Component({
   selector: 'app-list-data-mode',
@@ -19,7 +21,6 @@ export class ListDataModeComponent {
   @Input() addText: string;
   @Input() showAdd: boolean = true;
   @Input() isEditing: boolean = false;
-
   @Input() nameMode: string = '';
   @Input() creationDate: string = '';
   @Input() modificationDate: string = '';
@@ -29,37 +30,95 @@ export class ListDataModeComponent {
 
   @Output() ModeAdded: EventEmitter<void> = new EventEmitter<void>();
   @Output() updateMode = new EventEmitter<string>();
+  @Output() resetForm = new EventEmitter<void>();
 
   @ContentChild('templateData', { static: false})
   templateData: TemplateRef<any>;
 
   constructor(
-    private alertService: AlertService
+    private alertService: AlertService,
+    private modalidadService: ModalidadService
   ) { }
 
-  get isNameModeValid(): boolean {
-    return this.nameMode && this.nameMode.trim().length > 0;
+  private normalizeModeName(name: string): string {
+    return name.trim().replace(/\s+/g, ' ');
   }
 
-  addData() {
+  get isNameModeValid(): boolean {
+    return this.selectedNameMode && this.selectedNameMode.trim().length > 0;
+  }
 
-    if (!this.nameMode) {
-      this.alertService.alertError(
-        'El nombre de la modalidad puede estar vacío',
-      );
+  private validateModeName(name: string): boolean {
+    if (!name || name.trim().length === 0) {
+      this.alertService.alertWarning('El nombre de la modalidad no puede estar vacío');
+      return false;
+    }
+
+    const normalizedName = this.normalizeModeName(name);
+    const exists = this.data.some((mode: Modalidad) =>
+      this.normalizeModeName(mode.nombre).toLowerCase() === normalizedName.toLowerCase()
+    );
+
+    if (exists) {
+      this.alertService.alertWarning('Ya existe esta modalidad');
+      return false;
+    }
+
+    return true;
+  }
+
+  emitUpdateMode() {
+    const normalizedName = this.normalizeModeName(this.selectedNameMode);
+
+    if (!this.validateModeName(normalizedName)) {
       return;
     }
 
-    // const normalizedMode = this.nameMode.replace(/\s+/g, '').toLowerCase();
+    this.updateMode.emit(normalizedName);
+  }
 
-    // this.sqliteManager.nameModeExists(normalizedNameMode)
-    // .then((exists) => {
-    //   if (exists) {
-    //     this.alertService.alertWarning('Esta modalidad ya existe');
-    //     return;
-    //   }
-    // });
+  addData() {
+    const normalizedName = this.normalizeModeName(this.selectedNameMode);
 
+    if (!this.validateModeName(normalizedName)) {
+      return;
+    }
+
+    const newMode: Modalidad = {
+      nombre: normalizedName,
+      estatus: 1
+    };
+
+    this.alertService.alertConfirm(
+      '¿Estás seguro de agregar esta modalidad?',
+      () => {
+        this.modalidadService.createModalidad(newMode).subscribe({
+          next: () => {
+            this.alertService.alertSuccess('Modalidad agregada correctamente');
+            this.resetFormFields();
+            this.ModeAdded.emit();
+          },
+          error: async (err) => {
+            if (err.error && err.error.message) {
+              await this.alertService.alertWarning(err.error.message);
+              this.resetFormFields();
+            } else {
+              await this.alertService.alertError('Error al agregar la modalidad');
+            }
+          }
+        });
+      }
+    );
+  }
+
+  resetFormFields() {
+    this.selectedNameMode = '';
+    this.creationDate = '';
+    this.modificationDate = '';
+    this.deactivationDate = '';
+    this.isEditing = false;
+    this.isReadOnly = false;
+    this.resetForm.emit();
   }
 
 }
