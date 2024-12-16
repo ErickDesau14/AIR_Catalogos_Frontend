4b8e30a58607a8adc9aa204746de8735e31e7475
ErickDesau14 <erick_1322134389@uptecamac.edu.mx>
2024-12-16 17:28:18 -0600

    Añadiendo rutas de conexión del catálogo de notificaciones al enviroment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,6 +9,7 @@ export const environment = {
   puestoUrl: 'http://localhost:8080/api/v1/p',
   experienciaUrl: 'http://localhost:8080/api/v1/e',
   modalidadUrl: 'http://localhost:8080/api/v1/m',
+  notificacionUrl: 'http://localhost:8080/api/v1/n',
   empresaUrl: 'http://localhost:8080/api/v1/e',
   archivoUrl: 'http://localhost:8080/api/v1/a/recurso',
   evaluacionUrl: 'http://localhost:8080/api/v1/e/recurso'
