2a04480bcba7b1ccc24a75a1b9717095bcdee0c1
ErickDesau14 <erick_1322134389@uptecamac.edu.mx>
2024-12-16 23:46:56 -0600

    Desarrollo de funciones CRUD de tipo de notificaciones

--- src/app/pages/tipoNotificaciones/tab6/components/list-type-notifications/list-type-notifications.component.ts
+++ src/app/pages/tipoNotificaciones/tab6/components/list-type-notifications/list-type-notifications.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { IonContent } from '@ionic/angular';
-import { TipoNotificacciones } from 'src/app/models/typeNotifications';
+import { TipoNotificaciones } from 'src/app/models/typeNotifications';
 import { AlertService } from 'src/app/services/alert.service';
+import { TipoNotificationService } from 'src/app/services/typeNotification.service';
 
 @Component({
   selector: 'app-list-type-notifications',
@@ -12,22 +13,19 @@ export class ListTypeNotificationsComponent  implements OnInit {
 
   @ViewChild(IonContent) content: IonContent;
 
-  public typeNotifications: TipoNotificacciones[];
+  public typeNotifications: TipoNotificaciones[];
   public showForm: boolean;
-
-  public typeNotification: string = '';
   public selectedTypeNotification: string = '';
   public selectedCreationDate: string = '';
   public selectedModificationDate: string = '';
   public selectedDeactivationDate: string = '';
-
   public isEditing: boolean = false;
   public isReadOnly: boolean = false;
-
   public lastSelectedId: number | null = null;
 
   constructor(
-    private alertService: AlertService
+    private alertService: AlertService,
+    private tipoNotificacionesService: TipoNotificationService
   ) {
     this.showForm = false;
     this.typeNotifications = [];
@@ -38,62 +36,87 @@ export class ListTypeNotificationsComponent  implements OnInit {
   }
 
   getTypeNotifications() {
-
+    this.tipoNotificacionesService.getTipoNotificacion().subscribe({
+      next: (data) => {
+        this.typeNotifications = data;
+      },
+      error: async (error) => {
+        await this.alertService.alertError('Error al obtener los tipos de notificaciones ' + error.message);
+      }
+    });
   }
 
-  selectNotification(item: TipoNotificacciones, editMode: boolean = false) {
+  selectNotification(item: TipoNotificaciones, editMode: boolean = false) {
 
-    if (this.lastSelectedId === item.id) {
+    if (this.lastSelectedId === item.idTipoNotificacion) {
       this.resetForm();
-      this.lastSelectedId = null;
-      this.isReadOnly = false;
-    } else {
-      this.selectedTypeNotification = item.tipoNotificacion || '';
-      this.selectedCreationDate = item.fechaCreacion ? item.fechaCreacion.toLocaleDateString('en-CA') : '';
-      this.selectedModificationDate = item.fechaModificacion ? item.fechaModificacion.toLocaleDateString('en-CA') : '';
-      this.selectedDeactivationDate = item.fechaBaja ? item.fechaBaja.toLocaleDateString('en-CA') : '';
-
-      this.isEditing = editMode;
-      this.isReadOnly = !editMode;
-      this.lastSelectedId = item.id;
-      this.content.scrollToTop(500);
-
-      console.log("Selected Details:", {
-        tipoNotificacion: this.selectedTypeNotification,
-        creationDate: this.selectedCreationDate,
-        modificationDate: this.selectedModificationDate,
-        deactivationDate: this.selectedDeactivationDate
-      });
+      return;
     }
+
+    this.tipoNotificacionesService.getTipoNotificacionById(item.idTipoNotificacion).subscribe({
+      next: (typeNotification) => {
+        this.selectedTypeNotification = typeNotification.tipoNotificacion;
+        this.selectedCreationDate = typeNotification.fechaCreacion ? new Date(typeNotification.fechaCreacion).toISOString().split('T')[0] : '';
+        this.selectedModificationDate = typeNotification.fechaModificacion ? new Date(typeNotification.fechaModificacion).toISOString().split('T')[0] : '';
+        this.selectedDeactivationDate = typeNotification.fechaBaja ? new Date(typeNotification.fechaBaja).toISOString().split('T')[0] : '';
+        this.isEditing = editMode;
+        this.isReadOnly = !editMode;
+        this.lastSelectedId = item.idTipoNotificacion;
+        this.content.scrollToTop(500);
+      },
+      error: async (error) => {
+        await this.alertService.alertError('Error al obtener el tipo de notificación ' + error.message);
+      }
+    });
 
   }
 
   async updateTypeNotification(updatedText: string) {
-
-    if (!updatedText || updatedText.trim().length === 0) {
-      await this.alertService.alertError(
-        'El texto del tipo de la notificación no puede estar vacío'
-      );
-      return;
-    }
-
     if (this.lastSelectedId !== null) {
-      const updatedTypeNotification: TipoNotificacciones = {
-        id: this.lastSelectedId,
+      const updatedTypeNotification: TipoNotificaciones = {
         tipoNotificacion: updatedText.trim(),
-        estatus: 1,
-        fechaCreacion: this.selectedCreationDate ? new Date(this.selectedCreationDate) : null,
-        fechaModificacion: new Date(),
-        fechaBaja: this.selectedDeactivationDate ? new Date(this.selectedDeactivationDate) : null
+        estatus: 1
       };
 
       this.alertService.alertConfirm(
-        '¿Estás seguro de que deseas actualizar el tipo de notificación?',
+        '¿Está seguro de actualizar este tipo de notificación?',
         () => {
-
+          this.tipoNotificacionesService.updateTipoNotificacion(this.lastSelectedId, updatedTypeNotification).subscribe({
+            next: () => {
+              this.alertService.alertSuccess('Tipo de notificación actualizada correctamente');
+              this.getTypeNotifications();
+              this.resetForm();
+            },
+            error: () => {
+              this.alertService.alertError('Error al actualizar este tipo de notificación');
+            }
+          });
         }
       );
     }
+  }
+
+  desactivateTypeNotification(item: TipoNotificaciones) {
+    const newEstatus = item.estatus === 1 ? 0 : 1;
+    const mensaje = newEstatus === 1
+      ? '¿Está seguro de activar este tipo de notificación?'
+      : '¿Está seguro de desactivar este tipo notificación?';
+
+    this.alertService.alertConfirm(
+      mensaje,
+      () => {
+        this.tipoNotificacionesService.updateEstatus(item.idTipoNotificacion, newEstatus).subscribe({
+          next: () => {
+            this.alertService.alertOnOff(newEstatus);
+            this.getTypeNotifications();
+            this.resetForm();
+          },
+          error: async (error) => {
+            await this.alertService.alertError('Error al actualizar el estatus del tipo de notificación ' + error.message);
+          }
+        });
+      }
+    );
   }
 
   resetForm() {
@@ -101,17 +124,13 @@ export class ListTypeNotificationsComponent  implements OnInit {
     this.selectedCreationDate = '';
     this.selectedModificationDate = '';
     this.selectedDeactivationDate = '';
-
     this.isEditing = false;
     this.isReadOnly = false;
+    this.lastSelectedId = null;
   }
 
   onShowForm() {
     this.showForm = true;
-  }
-
-  desactivateTechnology(idTecnologia: number) {
-    this.resetForm();
   }
 
 }
