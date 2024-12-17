fef1294a80cc72204934e47df403f4e67bd079a0
ErickDesau14 <erick_1322134389@uptecamac.edu.mx>
2024-12-16 19:39:08 -0600

    Desarrollo de clases abc para notificaciones y nuevas variables de edición

--- src/app/pages/notificaciones/tab5/components/list-notifications/list-notifications.component.ts
+++ src/app/pages/notificaciones/tab5/components/list-notifications/list-notifications.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { IonContent } from '@ionic/angular';
 import { Notificaciones } from 'src/app/models/notifications';
 import { AlertService } from 'src/app/services/alert.service';
+import { NotificationService } from 'src/app/services/notification.service';
 
 @Component({
   selector: 'app-list-notifications',
@@ -14,20 +15,17 @@ export class ListNotificationsComponent  implements OnInit {
 
   public notifications: Notificaciones[];
   public showForm: boolean;
-
-  public textNotification: string = '';
   public selectedTextNotification: string = '';
   public selectedCreationDate: string = '';
   public selectedModificationDate: string = '';
   public selectedDeactivationDate: string = '';
-
   public isEditing: boolean = false;
   public isReadOnly: boolean = false;
-
   public lastSelectedId: number | null = null;
 
   constructor(
-    private alertService: AlertService
+    private alertService: AlertService,
+    private notificationService: NotificationService
   ) {
     this.showForm = false;
     this.notifications = [];
@@ -38,62 +36,89 @@ export class ListNotificationsComponent  implements OnInit {
   }
 
   getNotifications() {
-
+    this.notificationService.getNotificacion().subscribe({
+      next: (data) => {
+        this.notifications = data;
+      },
+      error: async (error) => {
+        await this.alertService.alertError('Error al obtener las notificaciones ' + error.message);
+      }
+    });
   }
 
   selectNotification(item: Notificaciones, editMode: boolean = false) {
 
-    if (this.lastSelectedId === item.id) {
+    if (this.lastSelectedId === item.idNotificacion) {
       this.resetForm();
-      this.lastSelectedId = null;
-      this.isReadOnly = false;
-    } else {
-      this.selectedTextNotification = item.texto || '';
-      this.selectedCreationDate = item.fechaCreacion ? item.fechaCreacion.toLocaleDateString('en-CA') : '';
-      this.selectedModificationDate = item.fechaModificacion ? item.fechaModificacion.toLocaleDateString('en-CA') : '';
-      this.selectedDeactivationDate = item.fechaBaja ? item.fechaBaja.toLocaleDateString('en-CA') : '';
-
-      this.isEditing = editMode;
-      this.isReadOnly = !editMode;
-      this.lastSelectedId = item.id;
-      this.content.scrollToTop(500);
-
-      console.log("Selected Details:", {
-        texto: this.selectedTextNotification,
-        creationDate: this.selectedCreationDate,
-        modificationDate: this.selectedModificationDate,
-        deactivationDate: this.selectedDeactivationDate
-      });
+      return;
     }
+
+    this.notificationService.getNotificacionById(item.idNotificacion).subscribe({
+      next: (notification) => {
+        this.selectedTextNotification = notification.texto;
+        this.selectedCreationDate = notification.fechaCreacion ? new Date(notification.fechaCreacion).toISOString().split('T')[0] : '';
+        this.selectedModificationDate = notification.fechaModificacion ? new Date(notification.fechaModificacion).toISOString().split('T')[0] : '';
+        this.selectedDeactivationDate = notification.fechaBaja ? new Date(notification.fechaBaja).toISOString().split('T')[0] : '';
+        this.isEditing = editMode;
+        this.isReadOnly = !editMode;
+        this.lastSelectedId = item.idNotificacion;
+        this.content.scrollToTop(500);
+      },
+      error: async (error) => {
+        await this.alertService.alertError('Error al obtener la notificación ' + error.message);
+      }
+    });
 
   }
 
   async updateNotification(updatedText: string) {
 
-    if (!updatedText || updatedText.trim().length === 0) {
-      await this.alertService.alertError(
-        'El texto de la notificación no puede estar vacío'
-      );
-      return;
-    }
-
     if (this.lastSelectedId !== null) {
-      const updatedNotification: Notificaciones = {
-        id: this.lastSelectedId,
+      const updatedMode: Notificaciones = {
         texto: updatedText.trim(),
-        estatus: 1,
-        fechaCreacion: this.selectedCreationDate ? new Date(this.selectedCreationDate) : null,
-        fechaModificacion: new Date(),
-        fechaBaja: this.selectedDeactivationDate ? new Date(this.selectedDeactivationDate) : null
+        estatus: 1
       };
 
       this.alertService.alertConfirm(
-        '¿Estás seguro de que deseas actualizar el texto de la notificación?',
+        '¿Está seguro de actualizar esta modalidad?',
         () => {
-
+          this.notificationService.updateNotificacion(this.lastSelectedId, updatedMode).subscribe({
+            next: () => {
+              this.alertService.alertSuccess('Notificación actualizada correctamente');
+              this.getNotifications();
+              this.resetForm();
+            },
+            error: () => {
+              this.alertService.alertError('Error al actualizar esta notificación');
+            }
+          })
         }
       );
     }
+
+  }
+
+  desactivateNotification(item: Notificaciones) {
+    const newEstatus = item.estatus === 1 ? 0 : 1;
+    const mensaje = newEstatus === 1
+      ? '¿Está seguro de activar este notificación?'
+      : '¿Está seguro de desactivar esta notificación?';
+
+    this.alertService.alertConfirm(
+      mensaje,
+      () => {
+        this.notificationService.updateEstatus(item.idNotificacion, newEstatus).subscribe({
+          next: () => {
+            this.alertService.alertOnOff(newEstatus);
+            this.getNotifications();
+            this.resetForm();
+          },
+          error: async (error) => {
+            await this.alertService.alertError('Error al actualizar el estatus de la notificación ' + error.message);
+          }
+        })
+      }
+    )
   }
 
   resetForm() {
@@ -101,17 +126,13 @@ export class ListNotificationsComponent  implements OnInit {
     this.selectedCreationDate = '';
     this.selectedModificationDate = '';
     this.selectedDeactivationDate = '';
-
     this.isEditing = false;
     this.isReadOnly = false;
+    this.lastSelectedId = null;
   }
 
   onShowForm() {
     this.showForm = true;
-  }
-
-  desactivateTechnology(idTecnologia: number) {
-    this.resetForm();
   }
 
 }
